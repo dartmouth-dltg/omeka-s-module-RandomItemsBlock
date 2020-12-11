3d25d654ecd414788ef678e115c075ce9818c654
Julian Maurice <julian.maurice@biblibre.com>
2020-12-11 11:21:43 +0100

    Use raw SQL queries as it is much more faster
    Also, fetch all entities with a single query

--- src/View/Helper/RandomItems.php
+++ src/View/Helper/RandomItems.php
@@ -2,6 +2,8 @@
 namespace RandomItemsBlock\View\Helper;
 
 use Doctrine\ORM\EntityManager;
+use Omeka\Api\Adapter\Manager as ApiAdapterManager;
+use Omeka\Entity\Item;
 use Zend\View\Helper\AbstractHelper;
 
 class RandomItems extends AbstractHelper
@@ -11,27 +13,49 @@ class RandomItems extends AbstractHelper
      */
     protected $entityManager;
 
-    public function __construct(EntityManager $entityManager)
+    /**
+     * @var ApiAdapterManager
+     */
+    protected $apiAdapterManager;
+
+    public function __construct(EntityManager $entityManager, ApiAdapterManager $apiAdapterManager)
     {
         $this->entityManager = $entityManager;
+        $this->apiAdapterManager =  $apiAdapterManager;
     }
 
-    public function __invoke(int $count)
+    /**
+     * @return \Omeka\Api\Representation\ItemRepresentation[]
+     */
+    public function __invoke(int $count): array
     {
         $em = $this->entityManager;
-        $api = $this->getView()->api();
 
-        $query = $em->createQuery("SELECT r.id FROM Omeka\Entity\Item r ORDER BY RAND()");
-        $query->setMaxResults($count);
+        $conn = $em->getConnection();
 
-        $items = [];
-        foreach ($query->getResult() as $result) {
-            $item = $api->read('items', $result['id'])->getContent();
-            if ($item) {
-                $items[] = $item;
-            }
+        $sql = 'SELECT id FROM resource';
+        $sql .= ' WHERE resource_type = :resourceType';
+
+        # Limit to public items so we don't have to check for user permissions
+        # (anyone can see public items even anonymous users)
+        $sql .= ' AND is_public = 1';
+
+        $sql .= ' ORDER BY RAND()';
+        $sql .= " LIMIT $count";
+
+        $stmt = $conn->prepare($sql);
+        $stmt->execute(['resourceType' => Item::class]);
+        $result = $stmt->fetchAll();
+        $itemIds = array_column($result, 'id');
+
+        $itemAdapter = $this->apiAdapterManager->get('items');
+
+        $items = $em->getRepository(Item::class)->findBy(['id' => $itemIds]);
+        $itemRepresentations = [];
+        foreach ($items as $item) {
+            $itemRepresentations[] = $itemAdapter->getRepresentation($item);
         }
 
-        return $items;
+        return $itemRepresentations;
     }
 }
