3fec1b84e87a71e7adaf00a9ca00c5d6804ed24c
Joshua Shaw <joshua.d.shaw@dartmouth.edu>
2022-05-26 16:52:56 -0400

    add additional Resource Template option to Random Item block

--- src/Site/BlockLayout/RandomItems.php
+++ src/Site/BlockLayout/RandomItems.php
@@ -7,6 +7,7 @@ use Omeka\Api\Representation\SitePageBlockRepresentation;
 use Omeka\Api\Representation\SitePageRepresentation;
 use Omeka\Site\BlockLayout\AbstractBlockLayout;
 use Laminas\Form\Element\Number;
+use Laminas\Form\Element\Select;
 use Laminas\View\Renderer\PhpRenderer;
 
 class RandomItems extends AbstractBlockLayout
@@ -26,7 +27,27 @@ class RandomItems extends AbstractBlockLayout
         ]);
         $count->setValue($block ? $block->dataValue('count', '3') : '3');
 
-        return $view->formRow($count);
+        // get the resource templates options
+        // we'll use this to narrow the SQL criteria
+        $data['sort_by'] = 'title';
+        $response = $view->api()->search('resource_templates', $data);
+        $resource_templates = $response->getContent();
+
+        $resource_templates_list = [];
+        foreach($resource_templates as $res_template) {
+            $resource_templates_list[$res_template->id()] = $res_template->label();
+        }
+
+        $resTemplateSelectedOption = $block ? $block->dataValue('show_res_template_select_option', '') : '';
+        $resTemplateSelect = new Select('o:block[__blockIndex__][o:data][show_res_template_select_option]');
+        $resTemplateSelect->setValueOptions($resource_templates_list)->setValue($resTemplateSelectedOption);
+        $resTemplateSelect->setLabel('Choose a resource template to select random items from');
+
+        $resTemplateSelect->setValue($block ? $block->dataValue('show_res_template_select_option', '0') : '0');
+
+        $formReturn = $view->formRow($resTemplateSelect);
+        $formReturn .= $view->formRow($count);
+        return $formReturn;
     }
 
     public function render(PhpRenderer $view, SitePageBlockRepresentation $block)
--- src/View/Helper/RandomItems.php
+++ src/View/Helper/RandomItems.php
@@ -27,13 +27,13 @@ class RandomItems extends AbstractHelper
     /**
      * @return \Omeka\Api\Representation\ItemRepresentation[]
      */
-    public function __invoke(int $count): array
+    public function __invoke(int $count, int $res_template_id = null): array
     {
         $em = $this->entityManager;
 
         $itemAdapter = $this->apiAdapterManager->get('items');
 
-        $items = $em->getRepository(Item::class)->findBy(['id' => $this->getRandomItemIds($count)]);
+        $items = $em->getRepository(Item::class)->findBy(['id' => $this->getRandomItemIds($count, $res_template_id)]);
         $itemRepresentations = [];
         foreach ($items as $item) {
             $itemRepresentations[] = $itemAdapter->getRepresentation($item);
@@ -42,7 +42,7 @@ class RandomItems extends AbstractHelper
         return $itemRepresentations;
     }
 
-    protected function getRandomItemIds($count, $useCache = true)
+    protected function getRandomItemIds($count, $res_template_id, $useCache = true)
     {
         $cacheKey = "omeka:RandomItems:randomItemIds:$count";
         $itemIds = $useCache ? $this->getFromCache($cacheKey) : false;
@@ -53,6 +53,10 @@ class RandomItems extends AbstractHelper
             $sql = 'SELECT id FROM resource';
             $sql .= ' WHERE resource_type = :resourceType';
 
+            if (isset($res_template_id)) {
+              $sql .= ' AND resource_template_id = :resourceTemplateId';
+            }
+
             // Limit to public items so we don't have to check for user permissions
             // (anyone can see public items even anonymous users)
             $sql .= ' AND is_public = 1';
@@ -61,7 +65,13 @@ class RandomItems extends AbstractHelper
             $sql .= " LIMIT $count";
 
             $stmt = $conn->prepare($sql);
-            $stmt->execute(['resourceType' => Item::class]);
+
+            if (isset($res_template_id)) {
+              $stmt->execute(['resourceType' => Item::class, 'resourceTemplateId' => $res_template_id]);
+            }
+            else {
+              $stmt->execute(['resourceType' => Item::class]);
+            }
             $result = $stmt->fetchAll();
             $itemIds = array_column($result, 'id');
 
